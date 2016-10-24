e7dbbb10015bee8cc290c1f04ba5b12357fd5d26
Morgan Empey <morgan.empey@gmail.com>
2016-10-24 16:49:40 -0600

    Stubbed out MainWindow based on our UML.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -11,4 +11,14 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateAnimation()
+{
+
+}
+
+void MainWindow::toggleGridDisplay()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <spriteframecollection.h>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void updateAnimation();
+
 private:
     Ui::MainWindow *ui;
+    SpriteFrameCollection frames;
+    QTimer animationTimer;
+
+    void toggleGridDisplay();
 };
 
 #endif // MAINWINDOW_H
