1642d69dae82831b87aeb6e2745ad68bbefcd9e1
JeeDragon <bra_nker@hotmail.com>
2016-10-24 20:01:13 -0600

    playing around with qwidgets

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,12 +4,23 @@
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QColorDialog>
+#include <QImage>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // TODO: Try to move to the group box.
+    QColorDialog *colorSelector = new QColorDialog(ui->widget);
+    colorSelector->setWindowFlags(Qt::Widget);
+    colorSelector->setOptions(QColorDialog::DontUseNativeDialog | QColorDialog::NoButtons);
+
+    QImage *image = new QImage();
+    ui->widget_2->setWindowFlags(Qt::Widget);
+    ui->scrollArea->setWidget(colorSelector);
 }
 
 MainWindow::~MainWindow()
