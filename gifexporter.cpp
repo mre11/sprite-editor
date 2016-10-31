bea4ad9377e3d9e1a6494605665a367ff56bad15
tominaga <Naoki@Naokis-MacBook-Pro.local>
2016-10-31 17:45:20 -0600

    implementing gifexporter.cpp

--- gifexporter.cpp
+++ gifexporter.cpp
@@ -7,10 +7,15 @@ void GifExporter::exportGif(std::string fileName, SpriteFrameCollection &frames)
     //Usage:
     //-------------------
     //Create a GifWriter struct.
-
+    GifWriter* gifwriter;
     //Pass the struct to GifBegin() to initialize values and write the file header.
-
+    GifBegin(gifwriter, fileName.c_str(), frames.getWidth(), frames.getHeight(), 10);
     //Pass frames of the animation to GifWriteFrame().
-
+    int numbFrames = frames.getNumbFrames();
+    for(int i = 0; i < numbFrames; i++)
+    {
+        //GifWriteFrame(gifwriter, frames.getFrame(i)->getImage(), frames.getWidth(), frames.getHeight(), 10);
+    }
     //Finally, call GifEnd() to close the file handle and free memory.
+    GifEnd(gifwriter);
 }
